da10d82736d5adfee00a0acde284cce5d87c7d44
balaji972011 <120183446+balaji972011@users.noreply.github.com>
2023-01-31 18:57:40 +0530

    select html elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 `use strict`;
+
+// select elements
+const formEl = document.getElementById(`form`);
+const usernameEl = document.getElementById(`username`);
+const emailEl = document.getElementById(`email`);
+const mobileEl = document.getElementById(`mobile`);
+const passwordEl = document.getElementById(`password`);
+const confirmPasswordEl = document.getElementById(`confirm-password`);
