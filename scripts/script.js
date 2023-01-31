eb1f3396c1375443cfa6774313d6a5032e84526e
balaji972011 <120183446+balaji972011@users.noreply.github.com>
2023-01-31 20:44:40 +0530

    js logic added

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,47 @@ const emailEl = document.getElementById(`email`);
 const mobileEl = document.getElementById(`mobile`);
 const passwordEl = document.getElementById(`password`);
 const confirmPasswordEl = document.getElementById(`confirm-password`);
+
+// events
+formEl.addEventListener(`submit`, (e) => {
+  e.preventDefault();
+  if (usernameEl.value === ``) {
+    const formControlEl = usernameEl.parentElement;
+    formControlEl.classList.add(`error`);
+  } else {
+    const formControlEl = usernameEl.parentElement;
+    formControlEl.classList.add(`success`);
+  }
+
+  if (emailEl.value === ``) {
+    const formControlEl = emailEl.parentElement;
+    formControlEl.classList.add(`error`);
+  } else {
+    const formControlEl = emailEl.parentElement;
+    formControlEl.classList.add(`success`);
+  }
+
+  if (mobileEl.value === ``) {
+    const formControlEl = mobileEl.parentElement;
+    formControlEl.classList.add(`error`);
+  } else {
+    const formControlEl = mobileEl.parentElement;
+    formControlEl.classList.add(`success`);
+  }
+
+  if (passwordEl.value === ``) {
+    const formControlEl = passwordEl.parentElement;
+    formControlEl.classList.add(`error`);
+  } else {
+    const formControlEl = passwordEl.parentElement;
+    formControlEl.classList.add(`success`);
+  }
+
+  if (confirmPasswordEl.value === ``) {
+    const formControlEl = confirmPasswordEl.parentElement;
+    formControlEl.classList.add(`error`);
+  } else {
+    const formControlEl = confirmPasswordEl.parentElement;
+    formControlEl.classList.add(`success`);
+  }
+});
